8c94dbdce3cfb08a16933f6466f951ce7f81fb8c
VilleKrumlinde <ville.krumlinde@gmail.com>
2023-04-27 14:34:40 +0200

    Audio now working.

--- makeplayer_mac.sh
+++ makeplayer_mac.sh
@@ -1,2 +1,2 @@
-fpc -Xm -XXis -O2 -FU/tmp -dMINIMAL -B -Mdelphi -FE./tools/ZDesigner -gw -gl -godwarfsets -k"-framework OpenGL -framework Cocoa -framework QuartzCore" zzdc.dpr
+fpc -Xm -XXis -O2 -FU/tmp -dMINIMAL -B -Mdelphi -FE./tools/ZDesigner -gw -gl -godwarfsets -k"-framework OpenGL -framework Cocoa -framework QuartzCore -framework AudioUnit" zzdc.dpr
 cp ./tools/ZDesigner/zzdc ./tools/ZDesigner/ZGameEditor.app/Contents/MacOS/player_macos.bin
